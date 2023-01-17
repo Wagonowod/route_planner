87933b2857bc611901972226b0ee6a4835cf0d78
wagonowod <kyzya95@bk.ru>
2023-01-17 23:52:28 +0500

    add docker

--- route_planner/settings.py
+++ route_planner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-82!u*=u=%lr4bh62=3wy2cc0i77kqm=-%cqp@_8-9r9!amu!q%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
